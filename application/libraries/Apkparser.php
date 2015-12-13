7df400e2f5339aff84b2d9bc2e2ddad60ac9700a
denzilferreira <ferreiradenzil@gmail.com>
2015-12-13 21:48:26 +0200

    Added android_sdk
    Add support for config parameter android_sdk

--- application/libraries/Apkparser.php
+++ application/libraries/Apkparser.php
@@ -5,19 +5,19 @@ class Apkparser {
     function __construct(){}
     
 	function getPackage($filepath) {
-        $package_name = exec(getcwd()."/aapt d badging " . getcwd().$filepath . " | grep package");
+        $package_name = exec($this->config->item('android_sdk')."build-tools/23.0.2/aapt d badging " . getcwd().$filepath . " | grep package");
         preg_match("/ name='([a-z0-9.]*)'/", $package_name, $matches);
         return $matches[1];
 	}
 	
 	function getVersion($filepath) {
-		$version = exec(getcwd()."/aapt d badging " . getcwd().$filepath . " | grep versionCode");
+		$version = exec($this->config->item('android_sdk')."build-tools/23.0.2/aapt d badging " . getcwd().$filepath . " | grep versionCode");
         preg_match("/versionCode='([0-9]*)'/", $version, $matches);
         return $matches[1];
 	}
 	
 	function getPermissions($filepath) {
-        exec(getcwd()."/aapt dump permissions " . getcwd().$filepath . " | grep uses-permission", $permissions );
+        exec($this->config->item('android_sdk')."build-tools/23.0.2/aapt dump permissions " . getcwd().$filepath . " | grep uses-permission", $permissions );
         $perms = array();
         if( count($permissions) > 0 ) {
           foreach( $permissions as $perm ) {
