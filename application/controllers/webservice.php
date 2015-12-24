72724f09ff0c4fa48becc77e5cdb5a784aeee3d9
denzilferreira <ferreiradenzil@gmail.com>
2015-12-24 22:25:59 +0200

    One MQTT connection per device

--- application/controllers/webservice.php
+++ application/controllers/webservice.php
@@ -382,17 +382,16 @@ class Webservice extends CI_Controller {
 			// Get MQTT server details
 			$mqtt_conf = $this->_get_mqtt_server_details($study_id);
 
-            // Using Mosquitto-PHP client that we installed over PECL
-            $client = new Mosquitto\Client("aware", false);
-            $client->setTlsCertificates($this->config->item("public_keys")."server.crt"); //load server SSL certificate
-            //$client->setTlsOptions(Mosquitto\Client::SSL_VERIFY_PEER, "tlsv1.2", NULL); //make sure peer has certificate
-            $client->setCredentials($mqtt_conf['mqtt_username'], $mqtt_conf['mqtt_password']); //load study-specific user credentials so we can connect
-            
 			// Get devices
 			$devices = $this->input->post('devices_list');
 			
 			// Loop through devices and send message
 			foreach	($devices as $device) {
+                // Using Mosquitto-PHP client that we installed over PECL
+                $client = new Mosquitto\Client("aware", false);
+                $client->setTlsCertificates($this->config->item("public_keys")."server.crt"); //load server SSL certificate
+                $client->setTlsOptions(Mosquitto\Client::SSL_VERIFY_PEER, "tlsv1.2", NULL); //make sure peer has certificate
+                $client->setCredentials($mqtt_conf['mqtt_username'], $mqtt_conf['mqtt_password']); //load study-specific user credentials so we can connect
                 $client->connect($mqtt_conf['mqtt_server'], $mqtt_conf['mqtt_port']); //make connection
                 $client->publish($topic['study_id'] . "/" . $device . "/" . $topic['type'], $msg, 1, false);
                 $client->disconnect();
