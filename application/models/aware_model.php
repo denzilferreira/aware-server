2e290246e40e140389ea6e4d46afddc23f42ba35
Denzil Ferreira <ferreiradenzil@gmail.com>
2017-01-26 10:17:01 +0200

    Poor MySQL indexing support...
    Fixed: IF NOT EXISTS for indexes is not supported in MySQL…

--- application/models/aware_model.php
+++ application/models/aware_model.php
@@ -210,7 +210,7 @@ foreach( $removed_columns as $column ) {
 				$database->query($query);
                 
                 //add a non unique index for faster queries based on time and device_id conditions
-                $query = 'CREATE INDEX IF NOT EXISTS time_device ON `'.$table.'` (timestamp, device_id)';
+                $query = 'CREATE INDEX time_device ON `'.$table.'` (timestamp, device_id)';
                 $database->query($query);
 			}
 		}
