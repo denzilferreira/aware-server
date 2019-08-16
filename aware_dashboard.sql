37d2d2ea3b1f08cd5570fe96e0988ca9d98ccaed
Denzil Ferreira <ferreiradenzil@gmail.com>
2019-08-16 09:26:04 +0300

    Update aware_dashboard.sql
    - Make user_agent text by default as the agent string is now larger than 50 characters.

--- aware_dashboard.sql
+++ aware_dashboard.sql
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS `ci_sessions`;
 CREATE TABLE `ci_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(16) NOT NULL DEFAULT '0',
-  `user_agent` varchar(50) NOT NULL,
+  `user_agent` TEXT NOT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
   `user_data` text NOT NULL,
   PRIMARY KEY (`session_id`)
